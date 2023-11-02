ab675e4c6d886c0fae2aa414dfc6c82c4f785d07
vimalapugazhan <p.vimalapugazhan@gmail.com>
2023-11-02 14:11:47 +0800

    changes1

--- tetris.srcs/sources_1/new/slow_drop.v
+++ tetris.srcs/sources_1/new/slow_drop.v
@@ -21,15 +21,23 @@
 
 
 module slow_drop(
-    input CLOCK,
+    input CLOCK, current,
     output reg [12:0] block_posy
     );
     wire clock2hz;
     flexi_clock_D(CLOCK, 500000000, clock2hz); // drops by 1 block every second
     
     always @ (posedge clock2hz)
-        begin
-            block_posy <= block_posy + 1;
+        begin 
+            if (current && block_posy >= 2 && block_posy <= 21)
+            begin
+                block_posy <= (block_posy == 21) ? 21 : block_posy + 1;
+            end
+            
+            if(!current)
+            begin
+                block_posy <= 2;
+            end
         end
     
 endmodule
