5b0ac7c1bd5e874359f7da7dd28899502074ee11
YJ Pan <yongjing.pan03@gmail.com>
2023-11-04 16:00:10 +0800

    Revert "remove old main"
    This reverts commit f7d69c4cc168281f2b71925020451c861f621fb8.

---
+++ tetris.srcs/sources_1/new/main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.10.2023 17:42:10
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+
+    );
+endmodule
